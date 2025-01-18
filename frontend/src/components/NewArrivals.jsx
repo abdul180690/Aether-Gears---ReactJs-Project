726918112bcd477e4790219890454915d022babb
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-19 03:28:14 +0530

    styling update

--- frontend/src/components/NewArrivals.jsx
+++ frontend/src/components/NewArrivals.jsx
@@ -1,65 +1,66 @@
-import React, { useContext, useEffect, useState } from 'react'
-import Title from './Title'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import { Autoplay } from 'swiper/modules'; 
-import { ShopContext } from '../context/ShopContext'; 
-import Item from './Item';
-
-
+import React, { useContext, useEffect, useState } from "react";
+import Title from "./Title";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
+import { ShopContext } from "../context/ShopContext";
+import Item from "./Item";
 
 const NewArrivals = () => {
-  const {products} = useContext(ShopContext)
-  const [PopularProducts, setPopularProducts] = useState([])
+  const { products } = useContext(ShopContext);
+  const [PopularProducts, setPopularProducts] = useState([]);
 
-  useEffect(()=> {
-  const data = products.slice(0, 7);
-  setPopularProducts(data)
-  }, [products])
+  useEffect(() => {
+    if (products && products.length > 0) {
+      const data = products.slice(0, 7); // Fetching only the first 7 products
+      setPopularProducts(data);
+    }
+  }, [products]);
 
   return (
-    <section className='max-padd-container pt-16'>
-      <Title 
-        title1={'New'} 
-        title2={' Arrivals'} 
-        titleStyles={'pb-10'} 
-        paraStyles={'!block'} 
+    <section className="max-padd-container pt-16 pb">
+      <Title
+        title1="New"
+        title2=" Arrivals"
+        titleStyles="pb-10 text-slate-900"
+        paraStyles="!block"
       />
-      
+
+      {/* Swiper for product slides */}
       <Swiper
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-      }}
-      breakpoints={{
-        300:{
-          slidesPerView: 2,
-          spaceBetween:30,
-        },
-        666:{
-          slidesPerView: 3,
-          spaceBetween:30,
-        },
-        900:{
-          slidesPerView: 4,
-          spaceBetween:30,
-        },
-        1300:{
-          slidesPerView: 5,
-          spaceBetween:30,
-        },
-      }}
+        autoplay={{
+          delay: 4000,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+          },
+          1440: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+          },
+        }}
         modules={[Autoplay]}
         className="h-[399px]"
       >
-        {PopularProducts.map((product)=>(
-        <SwiperSlide key={product._id}>
-          <Item product={product}/>
-        </SwiperSlide>
+        {PopularProducts.map((product) => (
+          <SwiperSlide key={product._id}>
+            <Item product={product} />
+          </SwiperSlide>
         ))}
       </Swiper>
     </section>
-  )
-}
+  );
+};
 
-export default NewArrivals
+export default NewArrivals;
