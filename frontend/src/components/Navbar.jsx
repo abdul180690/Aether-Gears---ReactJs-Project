fe3deda06602c70bfad0c8a90d9473f2d273abc6
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-19 03:23:30 +0530

    styling update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,32 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaHome, FaList, FaBlog, FaEnvelope } from "react-icons/fa";
 
-const Navbar = ({containerStyles, onClick}) => {
-const navLinks = [
-    {path: '/', title: 'Home'},
-    {path: '/collection', title: 'Collection'},
-    {path: '/blog', title: 'Blog'},
-    {path: 'mailto:info@aethergears.com', title: 'Contact'},
-]
+const Navbar = ({ containerStyles, onClick }) => {
+  const navLinks = [
+    { path: '/', title: 'Home', icon: <FaHome /> },
+    { path: '/collection', title: 'Collection', icon: <FaList /> },
+    { path: '/blog', title: 'Blog', icon: <FaBlog /> },
+    { path: 'mailto:info@aethergears.com', title: 'Contact', icon: <FaEnvelope /> },
+  ];
 
   return (
-    <nav className={`${containerStyles} boxShadow`}>
-        {navLinks.map((link)=>(
-            <NavLink
-            key={link.title}
-            to={link.path}
-            className={({isActive})=>`${isActive ? "active-link" : ""} px-3 py-2 rounded-full`}
-            onClick={onClick}
-            >
-            {link.title}
-
-            </NavLink>
-        ))}
+    <nav className={`${containerStyles}  flex gap-4`}>
+      {navLinks.map((link) => (
+        <NavLink
+          key={link.title}
+          to={link.path}
+          className={({ isActive }) =>
+            `${isActive ? 'active-link ' : 'text-gray-900'} flex items-center px-3 py-2 rounded-full hover:bg-primary-light hover:bg-slate-800 hover:text-primary  transition-all`
+          }
+          onClick={onClick}
+        >
+          <span className="mr-2">{link.icon}</span>
+          {link.title}
+        </NavLink>
+      ))}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
