fbdaa936d6839e204ecc35a8f5efbb1debfc137d
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-19 03:12:34 +0530

    Notification Component added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,17 +6,21 @@ import Collection from './pages/Collection'
 import Blog from './pages/Blog'
 import Product from './pages/Product'
 import BackToTop from "./components/BackToTop";
+import Footer from './components/Footer'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <main className='overflow-hidden text-tertiary'>
-      <Header />
+      <Notification />
+      <Header className="sticky top-0 z-50"/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} /> 
         <Route path='/blog' element={<Blog />} /> 
         <Route path='/product/:productId' element={<Product />} /> 
       </Routes>
+      <Footer />
       <BackToTop />
     </main>
   )
