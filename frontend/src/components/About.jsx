3fa94d278809fc52147fbe21361f63b6e9b1a1fb
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-19 03:15:29 +0530

    some  styling

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
     <section className='max-padd-container py-16'>
       <div className='flex flex-col md:flex-row gap-5 gap-y-10'>
         <div className='flex-[1] flexCenter flex-col'>
-          <Title title1={'People'} title2={' Says'} titleStyles={'h3'} titleStyles={'!pb-2'}/>
+          <Title title1={'People'} title2={' Says'} titleStyles={' h3 !pb-2 text-slate-900'}/>
           <img src={testimonial} alt="" height={55} width={55} className='rounded-full'/>
           <h4 className='h4 mt-6'>John Doe</h4>
           <p className='relative bottom-2'>CEO At TechStack</p>
@@ -22,12 +22,12 @@ const About = () => {
           <img src={about} alt="" className='rounded-2xl'/>
           <div className='absolute h-full w-full bg-white/20 top-0 left-0'/>
           <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white/80 p-6 rounded-xl'>
-            <h4 className='bold-18 text-center'>Top view in this <br />week</h4>
-            <h2 className='h2 uppercase'>Trending</h2>
+            <h4 className='bold-18 text-center text-slate-900'>Top view in this <br />week</h4>
+            <h2 className='h2 uppercase text-blue-950'>Trending</h2>
           </div>
         </div>
         <div className='flex-[1] flexCenter flex-col'>
-          <Title title1={'About'} title2={' Us'} titleStyles={'h3'} titleStyles={'!pb-2'} />
+          <Title title1={'About'} title2={' Us'} titleStyles={' h3 !pb-2 text-slate-900'} />
           <div className='flex flex-col items-start'>
             <div className='flexCenter gap-3 mb-3'>
                <RiSecurePaymentLine className='text-xl'/>
