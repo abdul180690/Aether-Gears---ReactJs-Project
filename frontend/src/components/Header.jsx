f64c3884f22eeae0d342a886d47b98600aaf3d1f
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-20 18:03:27 +0530

    styling update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import { FaBars, FaBarsStaggered } from "react-icons/fa6";
 import { IoMdCart } from "react-icons/io";
+import { TbUserCircle } from "react-icons/tb";
 import { RiUserLine } from "react-icons/ri";
 import logo from "../assets/icon.png";
 import { ShopContext } from "../context/ShopContext";
@@ -10,7 +11,7 @@ import { ShopContext } from "../context/ShopContext";
 
 const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
-  const {getCartCount} = useContext(ShopContext);
+  const {getCartCount, navigate} = useContext(ShopContext);
 
   const toggleMenu = () => setMenuOpened((prev) => !prev);
 
@@ -18,11 +19,12 @@ const Header = () => {
     <header className="max-padd-container w-full mb-2 bg-slate-800">
       <div className="flexBetween py-3">
         {/* Logo */}
-        <Link to={"/"} className="flex flex-1 bold-24 xl:bold-28 items-center text-white">
+        <Link to={"/"} className="flex flex-1  items-center text-white">
           <span className="me-3">
-            <img src={logo} alt="Logo" className="w-10 h-10" />
+            <img src={logo} alt="Logo" className="w-5 h-5" />
           </span>
-          Aether Gears
+          <span className="text-white uppercase tracking-widest">Aether Gears</span>
+          
         </Link>
 
         {/* Navbar */}
@@ -56,7 +58,7 @@ const Header = () => {
           <Link to={"/cart"} className="relative">
             <div className="flex px-3 bold-18 text-white">
               {/* <img src={cart} alt="Cart" className="w-12 h-12" /> */}
-              <IoMdCart className="text-5xl"/>
+              <IoMdCart className="text-3xl"/>
               <span className="bg-amber-200 ring-white ring-1 text-black text-[12px] font-semibold absolute -top-2 -right-0 flexCenter w-5 h-5 rounded-full shadow-lg">
                 {getCartCount()}
               </span>
@@ -65,7 +67,7 @@ const Header = () => {
 
           {/* Login Button */}
           <div className="relative">
-            <button className="btn-white flexCenter gap-x-2 hover:bg-gray-200 duration-300">
+            <button onClick={()=> navigate('/login')}className="btn-white flexCenter gap-x-2 hover:bg-gray-200 duration-300">
               Login
               <RiUserLine className="text-xl" />
             </button>
