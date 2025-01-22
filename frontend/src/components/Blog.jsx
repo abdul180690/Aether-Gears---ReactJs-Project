5640c9a38a9f2116d1e33f9de96736ddf1fc8e68
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-22 12:06:13 +0530

    styling update

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -6,7 +6,7 @@ const Blog = () => {
     <section className='max-padd-container pb-16'>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
         {blogs.slice(0, 4).map((blog)=>(
-          <div key={blog.title} className='relative border rounded-xl p-2'>
+          <div key={blog.title} className='relative border border-slate-400 rounded-xl p-2'>
             <img src={blog.image} alt="blogimg" className='rounded-xl'/>
             <p className='medium-14 mt-6'>{blog.category}</p>
             <h5 className='h5 pr-4 mb-1'>{blog.title}</h5>
