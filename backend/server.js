840cff0ed7269963b0047cfd251e6d862636f567
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-22 12:01:07 +0530

    updated with productRoute.js

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ import "dotenv/config"
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
+import productRouter from "./routes/productRoute.js"
 
 // App Config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
-connectCloudinary
+connectCloudinary()
 
 // Middleware
 app.use(express.json())
@@ -17,6 +18,8 @@ app.use(cors())
 
 // app endpoints
 app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
+
 app.get('/', (req, res) => {
     res.send('API Working')
 })
