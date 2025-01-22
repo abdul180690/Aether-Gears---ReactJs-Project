d330587a133f4f004af5dc5af4bfd05070703e72
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-23 04:15:24 +0530

    updated with page transition animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,53 @@
-import React from 'react'
-import Header from './components/Header'
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import Blog from './pages/Blog'
-import Product from './pages/Product'
-import BackToTop from "./components/BackToTop";
-import Footer from './components/Footer'
-import Notification from './components/Notification'
-import { ToastContainer } from 'react-toastify'
-import Cart from './pages/Cart'
-import PlaceOrder from './pages/PlaceOrder'
-import Login from './pages/Login'
+import React from 'react';
+import Header from './components/Header';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import Blog from './pages/Blog';
+import Product from './pages/Product';
+import BackToTop from './components/BackToTop';
+import Footer from './components/Footer';
+import Notification from './components/Notification'; // If you're using it, make sure it's implemented
+import { ToastContainer } from 'react-toastify';
+import Cart from './pages/Cart';
+import PlaceOrder from './pages/PlaceOrder';
+import Login from './pages/Login';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const App = () => {
+  const location = useLocation();  // Get the current location for page transition
+
   return (
-    <main className='overflow-hidden text-tertiary'>
+    <main className="overflow-hidden">
       <ToastContainer />
       {/* <Notification /> */}
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/collection' element={<Collection />} /> 
-        <Route path='/blog' element={<Blog />} /> 
-        <Route path='/product/:productId' element={<Product />} /> 
-        <Route path='/cart' element={<Cart />} /> 
-        <Route path='/place-order' element={<PlaceOrder />} /> 
-        <Route path='/login' element={<Login />} /> 
-      </Routes>
+      <Header />
+
+      {/* Transition Group for page fade animations */}
+      <TransitionGroup>
+        <CSSTransition
+          key={location.key} // Unique key for each location to trigger animations
+          timeout={500} // Duration of the transition
+          classNames="fade" // CSS class for animation
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/place-order" element={<PlaceOrder />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+
       <Footer />
       <BackToTop />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 // 7:46:54
