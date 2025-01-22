7850cd4a61e9a42d839f387050812c5183c922e0
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-22 12:07:19 +0530

    video removed from hero section, updated with image

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,25 +1,17 @@
 import React, { useRef } from 'react';
-import productVideo from '../assets/product-video.mp4'; 
-import headphones from '../assets/bg.png'
+import bg from '../assets/bg.png'
+import headphone from '../assets/headphone.png'
 import { FaArrowRightLong } from 'react-icons/fa6';
 
 
 const Hero = () => {
   return (
-    <section className="relative max-padd-container h-[633px]  overflow-hidden top-">
-      {/* Video Background */}
-      <video
-        src={productVideo}
-        className="absolute top-0 left-0 w-full h-full object-cover"
-        autoPlay
-        loop
-        muted
-        playsInline
-        preload="auto"
-      ></video>
+    <section className="mt-20 relative max-padd-container h-[633px]  overflow-hidden top-">
+     {/* Hero background Image */}
+      <img src={bg} alt="" className='absolute object-cover w-full h-full top-0 left-0 '/>
 
       {/* Overlay (Optional for Better Text Visibility) */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-20"></div>
 
       {/* Hero Content */}
       <div className="relative grid grid-cols-1 xs:grid-cols-2 h-full items-center">
@@ -28,7 +20,7 @@ const Hero = () => {
           <p className="text-white text-xl xs:text-2xl max-w-xs">
             Discover the best audio experience with cutting-edge technology and sleek designs.
           </p>
-          <button className="btn bg-secondary mt-6 rounded-full text-white px-6 py-2 hover:bg-primary hover:text-black duration-300">
+          <button className="btn bg-white mt-6 rounded-full text-black px-6 py-2 hover:bg-gray-300 duration-300">
             Explore More
           </button>
         </div>
@@ -36,10 +28,10 @@ const Hero = () => {
         {/* Right Side */}
         <div className="hidden xs:flex items-center justify-end p-6">
           <div className="flex flex-col items-center rounded-2xl w-[300px] p-4">
-            <div className='w-100 h-100'>
-              <img src= {headphones} alt="" className='rounded-3xl'/>
+            <div className='w-100 h-100 overflow-hidden rounded-3xl'>
+              <img src= {headphone} alt="" className='rounded-3xl zoom'/>
             </div>
-            <button className="btn flex items-center justify-center gap-2 mt-4 py-2 px-4 rounded-full text-white text-sm bg-secondary hover:bg-primary hover:text-black duration-300">
+            <button className="btn flex items-center justify-center gap-2 mt-4 py-2 px-4 rounded-full text-black text-sm bg-white hover:bg-gray-300 hover:text-black duration-300">
               Explore This Product <FaArrowRightLong />
             </button>
           </div>
