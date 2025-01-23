af68e2ca56d466972fcf7e12103d980de12a1bc2
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-23 10:03:37 +0530

    styling update

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Notification = () => {
   return (
-    <div className="flex flexbetween items-center bg-amber-100 text-sm text-gray-700 py-1 bg-opacity-50 relative">
+    <div className="flex flexbetween items-center bg-slate-300 text-slate-950 mb-6">
       {/* Free Shipping Section */}
       <div className="my-1 mx-6 text-center">
         <p className="text-secondary capitalize font-semibold">
-           <span className="font-extrabold text-red-500">FREE</span> shipping on orders{" "}
+           <span className="font-extrabold text-red-600">FREE</span> shipping on orders{" "}
           <span className="font-bold">₹1000</span> and up!{" "}
           <span className="underline text-xs lowercase font-bold hover:text-red-500">
             <Link>*details</Link>
@@ -19,8 +19,8 @@ const Notification = () => {
       {/* Black Friday Offer Section */}
       <div className="my-1 mx-6 text-center ">
         <p className="text-secondary capitalize font-semibold  rounded-xl mt-1 p-1">
-           <span className="uppercase font-extrabold text-red-500">black friday offer!</span> use
-          code <span className="font-extrabold text-red-500 uppercase text-xl animate-pulseGlow">20% off</span> at
+           <span className="uppercase font-extrabold text-red-600">black friday offer!</span> use
+          code <span className="font-extrabold text-red-600 uppercase text-xl text-pulse">"20% off"</span> at
           checkout to get a killer deal.{" "}
           <span className="underline text-xs lowercase font-bold hover:text-red-500">
             <Link>*details</Link>
@@ -32,10 +32,10 @@ const Notification = () => {
       {/* First Purchase Discount Section */}
       <div className="my-1 mx-6 text-center">
         <p className="text-secondary capitalize font-semibold">
-           <span className="font-extrabold uppercase text-red-500 ">10% off</span>  on
+           <span className="font-extrabold uppercase text-red-600 ">10% off</span>  on
           first purchase! {" "}
           <span className="capitalize underline text-xs font-bold hover:text-red-500">
-            <Link to={"/collection"}> Shop Now </Link>
+            <Link> *details </Link>
           </span>{" "}
           
         </p>
