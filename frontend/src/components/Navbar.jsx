e9db1bb1dbd4e37ea9ac383a037f4c1f389ac330
abdul180690 <abdullah180690.carrier@gmail.com>
2025-01-27 09:17:20 +0530

    updated navbar scroll effect

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,25 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { FaHome, FaList, FaBlog } from "react-icons/fa";
-import { FcAbout } from "react-icons/fc";
+import { FaHome, FaBloggerB } from "react-icons/fa";
+import { HiCollection } from "react-icons/hi";
+import { MdGroups } from "react-icons/md";
 
 
 const Navbar = ({ containerStyles, onClick }) => {
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [headerVisible, setHeaderVisible] = useState(true);
+  
+  const handleScroll = () => {
+    if (typeof window !== "undefined") {
+      const currentScrollY = window.scrollY;
+
+      if (currentScrollY > lastScrollY && currentScrollY > 200) {
+        setHeaderVisible(false);
+      } else if (currentScrollY < lastScrollY) {
+        setHeaderVisible(true);
+      }
+
+      setLastScrollY(currentScrollY);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+
   const navLinks = [
-    { path: '/', title: 'Home', icon: <FaHome /> },
-    { path: '/collection', title: 'Collection', icon: <FaList /> },
-    { path: '/blog', title: 'Blog', icon: <FaBlog /> },
-    { path: '/about', title: 'About Us', icon: <FcAbout className='text-white'/> },
+    { path: '/', title: 'Home', icon: <FaHome className='text-amber-400'/> },
+    { path: '/collection', title: 'Collection', icon: <HiCollection className='text-amber-400'/> },
+    { path: '/blog', title: 'Blog', icon: <FaBloggerB className='text-amber-400'/> },
+    { path: '/about-us', title: 'About Us', icon: <MdGroups className='text-xl text-amber-400'/> },
   ];
 
   return (
-    <nav className={`${containerStyles}  flex gap-4`}>
+    <nav className={`${containerStyles} ${
+      headerVisible ? "fixed top-0 left-50  z-40  mt-3 flex justify-center shadow-sm " : "-translate-y-full"
+    } flex gap-4`}>
       {navLinks.map((link) => (
         <NavLink
           key={link.title}
           to={link.path}
           className={({ isActive }) =>
-            `${isActive ? 'nav-underlined text-white' : 'text-white'} flex items-center hover:scale-105 px-2  focus:bold-18 transition-all duration-300`
+            `${isActive ? 'nav-underlined text-white duration-300' : 'text-white'} flex items-center  p-1 hover:bg-slate-100 hover:bg-opacity-30 hover:rounded-full hover:px-2   transition-all duration-300`
           }
           onClick={onClick}
         >
