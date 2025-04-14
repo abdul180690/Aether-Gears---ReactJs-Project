6b995730da81488f768934fabdafdac371f5d7da
abdul180690 <abdullah180690.carrier@gmail.com>
2025-04-14 06:56:33 +0530

    update profile route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,16 @@
-import express from "express"
-import { adminLogin, loginUser, registerUser } from "../controllers/userController.js"
+import express from "express";
+import {
+  adminLogin,
+  loginUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-userRouter.post('/register', registerUser)
-userRouter.post('/login', loginUser)
-userRouter.post('/admin', adminLogin)
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", adminLogin);
+userRouter.get("/profile", getUserProfile);
 
-export default userRouter
+export default userRouter;
