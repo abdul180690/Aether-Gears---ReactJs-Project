502103eaf5049ecb358917d07a2b41f5b3f4ec69
abdul180690 <abdullah180690.carrier@gmail.com>
2025-04-26 07:26:50 +0530

    backend-render config

--- backend/api/index.js
+++ backend/api/index.js
@@ -22,4 +22,9 @@ app.use("/api/wishlist", wishListRouter);
 app.use("/api/order", orderRouter);
 app.get("/", (req, res) => res.send("API Working"));
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 module.exports = app;
