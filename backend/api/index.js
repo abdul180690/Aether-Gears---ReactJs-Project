77b87a3c55c3d9668939a86123bff5c9a52aa9e4
abdul180690 <abdullah180690.carrier@gmail.com>
2025-04-26 07:09:46 +0530

    backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
 import connectDB from "../config/mongodb.js";
 import connectCloudinary from "../config/cloudinary.js";
 import userRouter from "../routes/userRoute.js";
@@ -9,24 +8,18 @@ import cartRouter from "../routes/cartRoute.js";
 import wishListRouter from "../routes/wishListRoute.js";
 import orderRouter from "../routes/orderRoute.js";
 
-const app = express();
+connectDB();
+connectCloudinary();
 
-(async () => {
-    await connectDB();
-    connectCloudinary();
-  
-    app.use(cors());
-    app.use(express.json());
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/wishlist", wishListRouter);
 app.use("/api/order", orderRouter);
-
-app.get("/", (req, res) => {
-    res.send("API is working!");
-  });
-})();
+app.get("/", (req, res) => res.send("API Working"));
 
 export default app;
